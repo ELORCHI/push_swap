cf0543c6a0594c94a60e5878ccabd2a92be0b29e
ELORCHI <elorchie@gmail.com>
2021-09-29 13:23:36 +0100

    add \n after printing

--- operations/swap.c
+++ operations/swap.c
@@ -6,7 +6,7 @@
 /*   By: eel-orch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/29 13:03:45 by eel-orch          #+#    #+#             */
-/*   Updated: 2021/09/29 13:04:03 by eel-orch         ###   ########.fr       */
+/*   Updated: 2021/09/29 13:22:52 by eel-orch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ void	swap(t_stack **stack, int id)
 	if (stack_len(*stack) < 2)
 		return ;
 	if (id == STACK_A)
-		ft_putstr("sa");
+		ft_putstr("sa\n");
 	else if (id == STACK_B)
-		ft_putstr("sb");
+		ft_putstr("sb\n");
 	tmp = (*stack)->data;
 	(*stack)->data = (*stack)->next->data;
 	(*stack)->next->data = tmp;
